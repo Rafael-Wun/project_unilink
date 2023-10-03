d6be18e705ec8d426d515e4c58833a1190786fb8
bluePuppy <rafael.825210075@stu.untar.ac.id>
2023-10-03 16:55:28 +0700

    exit commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:unilink/views/componentds/nav.dart';
+import 'package:unilink/views/components/nav.dart';
 
 void main() => runApp(StopwatchApp());
 
@@ -7,11 +7,12 @@ double deviceWidth = 0;
 double deviceHeight = 0;
 
 class StopwatchApp extends StatelessWidget {
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     deviceWidth = MediaQuery.of(context).size.width;
     deviceHeight = MediaQuery.of(context).size.height;
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Navbar(),
     );
   }
--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -8,17 +8,72 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('username'),
+        title: const Text('@mattloveblue'),
       ),
-      body: buildProfileBanner(),
+      body: ListView(
+        children: <Widget>[
+          buildHeader(),
+        ],
+      ),
     );
   }
 
-  Widget buildProfileBanner() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+  Widget buildHeader() {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
       children: [
-        Image.asset('assets/tori_gate.jpg', width: deviceWidth),
+        buildCoverImage(),
+        Positioned(
+          top: deviceHeight * .125,
+          child: buildProfile(),
+        ),
+      ],
+    );
+  }
+
+  Widget buildCoverImage() => Container(
+        color: Colors.grey,
+        child: Image.asset(
+          'assets/tori_gate.jpg',
+          width: deviceWidth,
+          height: deviceHeight * .2,
+          fit: BoxFit.cover,
+        ),
+      );
+
+  Widget buildProfile() {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage('assets/hyouka.jpg'),
+          radius: 64,
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          'Matthew Rene',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(
+          'Student of Tarumanagara University',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[400],
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        Text(
+          'Photographer | Basketball',
+          style: TextStyle(color: Colors.grey[400]),
+        ),
       ],
     );
   }
