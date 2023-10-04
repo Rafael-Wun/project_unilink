182e9416d69484810ecff1dcc930181bd26c464c
bluePuppy <rafael.825210075@stu.untar.ac.id>
2023-10-04 22:57:02 +0700

    exit commit

--- lib/views/pages/home_view.dart
+++ lib/views/pages/home_view.dart
@@ -10,8 +10,22 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  PostCard postCard1 = PostCard();
-  PostCard postCard2 = PostCard();
+  PostCard post1 = PostCard(
+    avatar:
+        'https://images.unsplash.com/photo-1600793575654-910699b5e4d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Z3RyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    caption: 'Lorem Ipsum',
+    photo:
+        'https://images.unsplash.com/photo-1602977050077-a669542f8dd8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGd0cnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    username: 'Anthony',
+  );
+  PostCard post2 = PostCard(
+    avatar:
+        'https://images.unsplash.com/photo-1591160690555-5debfba289f0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z29sZGVuJTIwcmV0cmlldmVyJTIwcHVwcHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    caption: 'Lorem Ipsum',
+    photo:
+        'https://images.unsplash.com/photo-1573074617613-fc8ef27eaa2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hldnJvbGV0JTIwY2FtYXJvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    username: 'Rafael',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +39,8 @@ class _HomeViewState extends State<HomeView> {
               padding: EdgeInsets.all(16.0),
               child: Column(children: [
                 CreatePost(),
-                postCard1,
-                postCard2,
+                post1,
+                post2,
               ]),
             ),
           ],
